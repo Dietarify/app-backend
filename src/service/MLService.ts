a7b62d19a8567c299dcebbc1d3d446cf04aef782
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2023-12-11 21:21:04 +0700

    adding token for request

--- src/service/MLService.ts
+++ src/service/MLService.ts
@@ -1,12 +1,32 @@
 import axios from 'axios';
 import logger from './log';
+import { GoogleAuth } from 'google-auth-library';
 
 const ML_API_URL = process.env.ML_API_URL ?? 'localhost:8000';
+const auth = new GoogleAuth();
+
+const mlService = axios.create();
+
+mlService.interceptors.request.use(
+  async (config) => {
+    logger.debug('setup request token');
+    const client = await auth.getIdTokenClient(ML_API_URL);
+    const token = await client.idTokenProvider.fetchIdToken(ML_API_URL);
+
+    config.headers.Authorization = token;
+    logger.debug(`token: ${token.slice(0, 5)}...`);
+
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
 
 export async function healthCheck() {
   const url = ML_API_URL;
   logger.info(`checking ml api health: ${url}`);
 
-  await axios.get(url);
+  await mlService.get(url);
   return true;
 }
